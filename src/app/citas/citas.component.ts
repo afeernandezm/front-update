adc67d9140f365622ea281124839d8303505a679
Antonio Fernández Marín <antonio.fernandez@Aytos.local>
2023-04-28 14:43:26 +0200

    commit con insert de citas, mostrar citas y casi modifica

--- src/app/citas/citas.component.ts
+++ src/app/citas/citas.component.ts
@@ -1,6 +1,8 @@
 import { ServiceService } from './../services/service.service';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { format } from 'date-fns';
+
 
 @Component({
   selector: 'app-citas',
@@ -9,15 +11,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitasComponent implements OnInit {
   gimnasios: any[] = [];
-
-  constructor(private http: HttpClient, private servicioService: ServiceService) { }
+  citas: any[] = [];
+  nombre_cliente:string="";
+  fecha_cita="";
+   /* fecha_formateada = this.fecha_cita.toLocaleDateString('es-ES'); */
+  hora_cita:string="";
+  nombre_gym:string="";
+  /* telefono_cliente:string=""; */
+  mostrarModal = false;
+  constructor(private http: HttpClient, private servicioService: ServiceService) {
+  }
 
   ngOnInit(): void {
     this.servicioService.getGimnasios()
       .subscribe((response: any[]) => {
         this.gimnasios = response;
       });
+
+      this.servicioService.getCitas()
+      ?.subscribe((response: any[]) => {
+        this.citas = response;
+      });
   }
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+  cerrarModal() {
+    this.mostrarModal = false;
+  }
+
+  isPastDate(dateString: string): boolean {
+    const today = new Date();
+    const rowDate = new Date(dateString);
+
+    return rowDate.getFullYear() < today.getFullYear() ||
+           rowDate.getMonth() < today.getMonth() ||
+           rowDate.getDate() < today.getDate();
+  }
+
+
+  registrarCita(): void {
+    const url = 'http://localhost:3000/citas';
+    const data = {
+      nombre_cliente: this.nombre_cliente,
+      fecha_cita: this.fecha_cita,
+      hora_cita: this.hora_cita,
+      nombre_gym: this.nombre_gym,
+      /* telefono_cliente: this.telefono_cliente, */
+    };
+    console.log(data);
+    this.http.post(url, data).subscribe(
+      (response) => {
+        console.log(response);
+        const formulario = document.getElementById('citaModal') as HTMLFormElement;
+        const alertSuccess = document.createElement('div');
+        alertSuccess.classList.add('alert', 'alert-success');
+        alertSuccess.textContent = ("Cita creada con éxito");
+        formulario.insertBefore(alertSuccess, formulario.firstChild);
+      location.reload();
+      },
+      (error) => {
+        console.error(error);
+
+      }
+    );
+  }
+
+
+  editarCita(): void {
+    //falta el id de la cita
+    const url = `http://localhost:3000/citas/`;
+    const data = {
+      fecha_cita: this.fecha_cita,
+      hora_cita: this.hora_cita
+    };
+    console.log(data);
+    this.http.put(url, data).subscribe(
+      (response) => {
+        console.log(response);
+        const formulario = document.getElementById('editarCitaModal') as HTMLFormElement;
+        const alertSuccess = document.createElement('div');
+        alertSuccess.classList.add('alert', 'alert-success');
+        alertSuccess.textContent = ("Cita actualizada con éxito");
+        formulario.insertBefore(alertSuccess, formulario.firstChild);
+        location.reload();
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+
 }
 
 
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -11,4 +11,21 @@ export class ServiceService {
   getGimnasios() {
     return this.http.get<any[]>('http://localhost:3000/gimnasios');
   }
+
+  getCitas() {
+    const clienteString = localStorage.getItem('cliente');
+    const cliente = clienteString ? JSON.parse(clienteString) : null;
+
+    if (cliente) {
+      const id_cliente = cliente.id_cliente.toString();
+      console.log('ID del cliente:', id_cliente);
+      return this.http.get<any[]>(`http://localhost:3000/get-citas/${id_cliente}`);
+    } else {
+      console.log('No se encontró el objeto cliente en el almacenamiento local');
+      return null; // o devuelve un Observable vacío, dependiendo de lo que necesites en tu aplicación
+    }
+
+}
+
+
 }
