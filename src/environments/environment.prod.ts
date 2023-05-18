011fd053febb15c1002f4120cf0dcb4e73132901
Antonio Fernández Marín <antonio.fernandez@Aytos.local>
2023-05-18 13:34:35 +0200

    memoria angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,10 @@ export const environment = {
   apiUrl: 'http://localhost:3000/portalGym',
   production: true,
   URL : {
-    citas:"https://backend-portalgym.onrender.com/portalGym/citas/",
-    ejercicios:"https://backend-portalgym.onrender.com/portalGym/ejercicios/",
-    gimnasios: "https://backend-portalgym.onrender.com/portalGym/gimnasios/",
-    usuarios:"https://backend-portalgym.onrender.com/portalGym/usuarios/"
+    citas:"https://backend-sb-portalgym.onrender.com/portalGym/citas/",
+    ejercicios:"https://backend-sb-portalgym.onrender.com/portalGym/ejercicios/",
+    gimnasios: "https://backend-sb-portalgym.onrender.com/portalGym/gimnasios/",
+    direcciones: "https://backend-sb-portalgym.onrender.com/portalGym/direcciones/",
+    usuarios:"https://backend-sb-portalgym.onrender.com/portalGym/usuarios/"
   }
 };
